9d44fa88c72ca61f26b3f42a9ba44821aa3c9cac
iliakri <45390420+iliakri@users.noreply.github.com>
2019-08-14 17:46:01 +0300

    create config files

--- conftest.py
+++ conftest.py
@@ -1,9 +1,23 @@
 import pytest
+import json
 
 from fixture.application import Application
+from os.path import join, dirname, abspath
+
+target = None
 
 
 @pytest.fixture(scope="session")
-def ppf_prod():
-    ppf_prod = Application("http://10.8.1.11:50020/api/v1")
-    return ppf_prod
+def app(request):
+    global target
+    if target is None:
+        config_file = join(dirname(abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+
+    fixture = Application(host=target['apiUrl'])
+    return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--target", action="store", default="ppf_prod.json")
