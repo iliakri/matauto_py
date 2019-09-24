b10370ab4ae8d0c5691e2f799f1f8df605150ea5
iliakri <45390420+iliakri@users.noreply.github.com>
2019-09-24 18:14:46 +0300

    create test_get_productions

--- test/transporters_test.py
+++ test/transporters_test.py
@@ -21,8 +21,17 @@ def test_get_shift(app, shift_id):
 
 @pytest.mark.get
 @pytest.mark.parametrize("transporter_id", (1, 2, 3, 4))
-def test_cameras(app, transporter_id):
+def test_get_cameras(app, transporter_id):
     res = app.workshops.get_cameras_by_transporter(transporter_id)
     assert res.status_code == 200
     assert res.headers['Content-Type'] == "application/json"
     app.schemas.assert_valid_schema(res.json(), 'cameras.json')
+
+
+@pytest.mark.get
+@pytest.mark.parametrize("transporter_id", (1, 2, 3, 4))
+def test_get_productions(app, transporter_id):
+    res = app.workshops.get_productions_by_transporter(transporter_id)
+    assert res.status_code == 200
+    assert res.headers['Content-Type'] == "application/json"
+    app.schemas.assert_valid_schema(res.json(), 'productions.json')
