a34822fa69013c55774ab49b395aafa7fc2fd63e
iliakri <45390420+iliakri@users.noreply.github.com>
2019-10-04 12:32:00 +0300

    edit hats and worker tests

--- test/hats_test.py
+++ test/hats_test.py
@@ -11,6 +11,11 @@ def test_get_hats_by_workshop(app):
     app.schemas.assert_valid_schema(res.json(), 'hats.json')
 
 
+def test_download_hats_by_workshop(app):
+    res = app.workshops.download_hats_by_workshop(1)
+    assert res.status_code == 200
+
+
 @pytest.mark.get
 def test_negative_get_hats_by_workshop(app):
     res = app.workshops.get_hats_by_workshop(1000)
--- test/workers_test.py
+++ test/workers_test.py
@@ -7,11 +7,12 @@ from data.worker import data_for_create_worker
 
 @pytest.mark.get
 def test_get_worker_by_id(app, db):
-    with allure.step("getting worker_id from db"):
+    with allure.step("Получаем worker_id из БД"):
         worker_id = random.choice(db.get_worker_id())
-    with allure.step("get worker with worker_id=%s from api" % worker_id):
+    with allure.step("Получаем работника с worker_id=%s через API" % worker_id):
         res = app.workshops.get_worker_by_workshop(1, worker_id)
-    print(json.dumps(res.json(), ensure_ascii=False, indent=2))
+    if res.status_code != 200:
+        allure.attach(json.dumps(res.json(), ensure_ascii=False, indent=2), "Response", "application/json")
     assert res.status_code == 200
     assert res.headers['Content-Type'] == "application/json"
     app.schemas.assert_valid_schema(res.json(), 'worker_by_workshop.json')
@@ -21,6 +22,8 @@ def test_get_worker_by_id(app, db):
 @pytest.mark.get
 def test_get_workers_by_workshop(app):
     res = app.workshops.get_workers_by_workshop(1)
+    if res.status_code != 200:
+        allure.attach(json.dumps(res.json(), ensure_ascii=False, indent=2), "Response", "application/json")
     assert res.status_code == 200
     assert res.headers['Content-Type'] == "application/json"
     app.schemas.assert_valid_schema(res.json(), 'workers_by_workshop.json')
