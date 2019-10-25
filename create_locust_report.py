4341054bc61fba253f82ea203ffb1b568f41856c
krivonosov <krivonosov@matller.com>
2019-10-25 15:42:02 +0300

    create chart by locust report

---
+++ create_locust_report.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from yattag import Doc
+import datetime
+
+plt.style.use('ggplot')  # Красивые графики
+plt.rcParams['figure.figsize'] = (15, 5)
+
+
+def autolabelh(rects):
+    for rect in rects:
+        width = rect.get_width()
+        plt.text(width * 1.05, rect.get_y() + rect.get_height() / 2., '%d' % int(width), ha='left', va='center', fontdict={'size': 10})
+
+
+def test_create_locust_chart():
+    current_datetime = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    data_csv = pd.read_csv("test_result_requests.csv", sep=',')
+    sorted_data = data_csv.sort_values(by=['Median response time'])
+    name, median_response_time = sorted_data['Name'], sorted_data['Median response time']
+    bar = plt.barh(range(len(median_response_time)), median_response_time, align='edge', alpha=0.7)
+    plt.yticks(range(len(name)), name, ha='right', va='bottom', size='small')
+    plt.subplots_adjust(left=0.3)
+    plt.grid(True)
+    autolabelh(bar)
+    title = current_datetime + ' Load Test Result'
+    plt.suptitle(title, fontsize=12, weight='bold')
+    plt.xlabel('Median Response Time(ms)')
+    plt.savefig('locust_report.png', bbox_inches='tight')
+
+
+def test_create_html_report():
+    doc, tag, text = Doc().tagtext()
+    with tag('html'):
+        with tag('body', id='img'):
+            with tag('p'):
+                doc.stag('img', src='locust_report.png')
+    output = open("load.html", 'w')
+    output.write(doc.getvalue())
+    output.close()
--- locustfile.py
+++ locustfile.py
@@ -6,28 +6,32 @@ class UserBehavior(TaskSet):
     @task(6)
     def sender_productions(l):
         data = {"unixtime": 0, "summ_out": 666666}
-        l.client.post("/sender/workshops/transporters/1/zones/5/productions", json=data)
+        l.client.post("/api/v1/sender/workshops/transporters/1/zones/5/productions", json=data)
 
     @task(4)
     def sender_hats(l):
         data = [{"zone_sub_id": 6, "hat_id": 30, "transporter_id": 1}]
-        l.client.post("/sender/workshops/1/hats", json=data)
+        l.client.post("/api/v1/sender/workshops/1/hats", json=data)
 
     @task(2)
     def zones_status(l):
-        l.client.get("/workshops/transporters/1/zones/status")
+        l.client.get("/api/v1/workshops/transporters/1/zones/status")
 
     @task(2)
     def get_cameras(l):
-        l.client.get("/workshops/transporters/4/cameras")
+        l.client.get("/api/v1/workshops/transporters/4/cameras")
 
     @task(1)
     def get_hats(l):
-        l.client.get("/workshops/1/hats/")
+        l.client.get("/api/v1/workshops/1/hats/")
 
     @task(1)
     def get_list_workers(l):
-        l.client.get("/workshops/1/workers/pages?direction=ASC&field=id&page=1&per_page=10")
+        l.client.get("/api/v1/workshops/1/workers/pages?direction=ASC&field=id&page=1&per_page=10")
+
+    @task(1)
+    def get_list_report(l):
+        l.client.get("/api/v1/workshops/transporters/1/workers/status?start_date=2019-10-14")
 
 
 class WebsiteUser(HttpLocust):
