e963ebab6ba3aa22a004efbb4a5365ee61799a70
ilia <ilyaalkri@gmail.com>
2019-12-19 16:59:34 +0300

    delete login_required
    add get_user_by_id

--- fixture/users.py
+++ fixture/users.py
@@ -1,13 +1,22 @@
-import allure
 import requests
 import json
+import allure
 
 
-class SessionHelper:
+class UsersHelper:
     s = requests.session()
 
     def __init__(self, app):
         self.app = app
+
+    def get_user_by_id(self, user_id: int):
+        return requests.get(self.app.host + f'/users/{user_id}')
+
+    def create_user(self, user):
+        return requests.post(self.app.host + '/users', json=user)
+
+    def get_user_if_login(self):
+        return self.s.get(self.app.host + '/users/authorization')
 
     def login(self, username, password):
         data = {"username": username, "password": password}
@@ -16,15 +25,11 @@ class SessionHelper:
     def logout(self):
         return self.s.delete(self.app.host + '/users/authorization')
 
-    def get_user_if_login(self):
-        return self.s.get(self.app.host + '/users/authorization')
-
-    def login_required(self):
-        return self.s.put(self.app.host + '/users/authorization')
-
+    @allure.step("Авторизация")
     def authorize(self, username, password):
         res = self.login(username, password)
         if res.status_code != 201:
             allure.attach(json.dumps(res.json(), ensure_ascii=False, indent=2), "Response", "application/json")
         return self.s
 
+57
