7aa8505942ffc692c24378b00eb759cd3dd1cf25
ilia <ilyaalkri@gmail.com>
2020-01-09 17:11:02 +0300

    create new method for users

--- fixture/users.py
+++ fixture/users.py
@@ -9,21 +9,27 @@ class UsersHelper:
     def __init__(self, app):
         self.app = app
 
-    @allure.step("Авторизация")
+    @allure.step("Authorization save")
     def authorize(self, username, password):
         res = self.login(username, password)
         if res.status_code != 201:
             allure.attach(json.dumps(res.json(), ensure_ascii=False, indent=2), "Response", "application/json")
         return self.s
 
-    def get_user_by_id(self, user_id: int):
-        return self.s.get(self.app.host + f'/users/{user_id}')
-
     def get_all_users(self):
         return self.s.get(self.app.host + '/users')
 
     def create_user(self, user):
         return self.s.post(self.app.host + '/users', json=user)
+
+    def get_user_by_id(self, user_id: int):
+        return self.s.get(self.app.host + f'/users/{user_id}')
+
+    def update_user(self, user_id: int, user):
+        return self.s.put(self.app.host + f'/users/{user_id}', json=user)
+
+    def delete_user(self, user_id: int):
+        return self.s.delete(self.app.host + f'/users/{user_id}')
 
     def get_user_if_login(self):
         return self.s.get(self.app.host + '/users/authorization')
