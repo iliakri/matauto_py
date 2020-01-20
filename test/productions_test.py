230b6b2917ccd4d16b7248915e07e9fd184b044f
ilia <ilyaalkri@gmail.com>
2020-01-20 19:10:45 +0300

    added test_delete_production

--- test/productions_test.py
+++ test/productions_test.py
@@ -19,3 +19,25 @@ def test_get_production_by_id(api, db, cookies):
         res = api.productions.get_production_by_id(production_id, cookies)
     api.assertion.status_code(res, [200])
     assert str(res.json().get("id")) == str(production_id)
+
+
+@pytest.mark.post
+def test_delete_production(api, db, cookies):
+    transporter_id = 4
+    with step("add production to transporter if none"):
+        if len(db.get_production_id_by_transporter(transporter_id)) == 0:
+            add_production = api.transporters.add_production_to_transporter(transporter_id, 1, cookies)
+            api.assertion.status_code(add_production, [200])
+    old_productions = db.get_production_id_by_transporter(transporter_id)
+    production_id = random.choice(old_productions)
+    res = api.transporters.del_production_from_transporter(transporter_id, production_id, cookies)
+    api.assertion.status_code(res, [200])
+    new_productions = db.get_production_id_by_transporter(transporter_id)
+    assert len(old_productions) - 1 == len(new_productions)
+
+
+
+'''    list_production = api.transporters.get_productions_by_transporter(6).json()
+    list_production_id = []
+    for i in range(len(list_production)):
+        list_production_id.append(list_production[i]['id'])'''
