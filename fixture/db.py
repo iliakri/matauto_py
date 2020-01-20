71cea3d071eec85583569513b49099fe3faecb48
ilia <ilyaalkri@gmail.com>
2020-01-20 19:10:04 +0300

    added methods for get transporters from bd

--- fixture/db.py
+++ fixture/db.py
@@ -1,4 +1,6 @@
 import psycopg2
+import pytest
+
 from model.worker import Worker
 
 
@@ -34,7 +36,7 @@ class DbFixture:
             for row in cursor:
                 worker_id = row[0]
                 list.append(worker_id)
-                #list.append(Worker(worker_id=worker_id))
+                # list.append(Worker(worker_id=worker_id))
         finally:
             cursor.close()
         return list
@@ -50,3 +52,27 @@ class DbFixture:
         finally:
             cursor.close()
         return productions
+
+    def get_production_id_by_transporter(self, transporter_id):
+        productions = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(f"select production_id from transporter_production where transporter_id={transporter_id}")
+            for row in cursor:
+                production_id = row[0]
+                productions.append(production_id)
+        finally:
+            cursor.close()
+        return productions
+
+    def get_all_transporters_id(self):
+        transporters = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(f"select id from transporter")
+            for row in cursor:
+                transporter_id = row[0]
+                transporters.append(transporter_id)
+        finally:
+            cursor.close()
+        return transporters
