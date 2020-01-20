afe317635ace509e768e70577a27f5a4f78227ed
ilia <ilyaalkri@gmail.com>
2020-01-20 19:01:06 +0300

    created data of transporters and workers

---
+++ data/transporters.py
@@ -0,0 +1,7 @@
+from model.transporter import Transporter
+
+testdata = [
+    Transporter(name="Мясо", id="555"),
+    Transporter(name="Рыба", id="666"),
+    Transporter(name="Кости", id="777"),
+    ]
